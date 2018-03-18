3310e8b06f38fecdc402d7be453c33ffb63db37a
Tars\Администратор <iwankozlov1989@gmail.com>
2018-03-18 21:34:51 +0200

    first commit

---
+++ PropertyFilesGenerator/File.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyFilesGenerator
+{
+    class File
+    {
+        protected String userName = getUserName();
+        protected String name;
+        protected String path;
+        protected String templateFilePath;
+        protected String fileText = getFileText();
+
+        protected String[] variablesArray;
+        protected Dictionary<String, String> environmentElements = new Dictionary<String, String>();
+
+        environmentElements.Add("dataBaseName", "sdaasd");
+
+        protected String getStringFromFile()
+        {
+            return;
+        }
+
+        protected String replaceStringWith()
+        {
+            return;
+        }
+
+        protected String getUserName()
+        {
+            return Environment.UserName;
+        }
+
+        protected void createFile()
+        {
+            using (FileStream fs = File.Create(path + name));
+        }
+
+        protected void saveFile(path)
+        {
+            
+        }
+
+        protected void getFileText()
+        {
+            return File.ReadAllText(path + name);
+        }
+
+        protected void findStringInFile()
+        {
+
+        }
+
+        protected void replaceStringWith(String toReplace, String replaceWith)
+        {
+            fileText.Replace(toReplace, replaceWith);
+        }
+
+
+
+
+
+    }
+}
---
+++ PropertyFilesGenerator/Page.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyFilesGenerator
+{
+    class Page
+    {
+
+        protected String pageURL;
+        protected String pageHTML;
+
+        protected String spp;
+        protected String tcl;
+        protected String scs;
+        protected String dbName;
+        protected String dbServer;
+
+        protected String getPageHTML()
+        {
+           
+        }
+
+        protected String getElementByXpath(String xPath)
+        {
+
+        }
+
+        protected String getElementsAttribute(String element, String attribute)
+        {
+
+        }
+
+    }
+}
